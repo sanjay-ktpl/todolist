87b5ab7290a51f1816589e68ef42ff41d85d6070
sanjay <sanjay.vyas@krishtechnolabs.com>
2024-02-16 16:36:50 +0530

    todolist

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css'; 
+import ToDoList from "./Components/toDoList"; 
+function App() {
+  return (
+    <div className="Myapp"> 
+      <ToDoList /> 
+    </div>
+  );
+}
+
+export default App;
